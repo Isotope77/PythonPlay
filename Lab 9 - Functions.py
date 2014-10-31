aa44933bc5d7369ed1751613d394f8107c39c030
darknight114 <gregg.neville@gmail.com>
2014-10-30 21:39:50 -0400

    Added Lab 9 - Part 4 -Pt. 1

--- Lab 9 - Functions.py
+++ Lab 9 - Functions.py
@@ -1,5 +1,16 @@
+import random
+
 # Part 3
 def find(list, key):
     for item in range(len(list)):
         if list[item] == key:
             print("Found ", key," at position ",item)
+
+# Part 4 - Pt. 1
+def create_list(list_size):
+    list = []
+    for i in range(list_size):
+        list.append(random.randrange(6))
+    return list
+my_list = create_list(5)
+print(my_list)
